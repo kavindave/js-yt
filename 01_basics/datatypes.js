708fd6c7b127c4bd44ee12ed1c886dacb1cd0833
kavin dave <kavindave2001@gmail.com>
2025-02-25 12:37:16 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,27 @@
+"use strict";// treat all js code as newer version
+
+// alert(3+3) // we are usind node js not browser
+
+// code readbility should be high
+
+let name = "kavin"
+let age  = "23"
+let isloggedIn = false
+let state;
+
+
+// datatypes:
+// number => 2 to power of 53
+// bigint 
+// string => ""
+// boolean => true or false
+// null => standalone value
+// undefined =>
+// symbol => uniqueness 
+
+
+// object 
+
+console.log(typeof null);// object 
+
+
